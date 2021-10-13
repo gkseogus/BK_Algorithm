0eef4ad4ac68a488c542621bb18f691f154e27e0
HDH <fbznffldj998@naver.com>
2021-10-13 23:23:20 +0900

    for문 1차수정

---
+++ for문 연습/10950.py
@@ -0,0 +1,7 @@
+t = int(input())
+
+for i in range(t):
+    a,b = input().split()
+    a = int(a)
+    b = int(b)
+    print(a+b)
---
+++ for문 연습/11022.py
@@ -0,0 +1,7 @@
+t = int(input())
+
+for i in range(1,t+1):
+    a,b = input().split()
+    a = int(a)
+    b = int(b)
+    print("Case #%s: %s + %s = %s"%(i,a,b,a+b))
